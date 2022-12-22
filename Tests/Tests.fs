13d8d507fae44f5e847c38fe4ab681f3381dd142
Fraser Waters <frassle@gmail.com>
2022-12-22 07:50:56 +0000

    tuple test

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -992,3 +992,85 @@ let ``Test object const property`` () =
     Test.fromJson """{}"""
     |> conversion.Reader
     |> Test.shouldEqual (Pulumi.Provider.PropertyValue ImmutableDictionary.Empty)
+
+    
+[<Fact>]
+let ``Test tuple additionalItems`` () =
+    let schema = System.Text.Json.JsonDocument.Parse """{
+        "type": "array",
+        "prefixItems": [
+            { "type": "string" },
+            { "type": "number" }
+        ],
+        "items": { "type": "boolean" }
+    }"""
+    let conversion = Provider.convertSchema Test.baseUri schema.RootElement
+    
+    conversion
+    |> Test.conversionToJson
+    |> Test.shouldJsonEqual (Test.complexSchema [
+        "schema:index:root", """{
+            "type": "object",
+            "properties": {
+                "item1": {
+                    "type": "string"
+                },
+                "item2": {
+                    "type": "number"
+                },
+                "additionalItems": {
+                    "type": "array",
+                    "items": {
+                        "type": "boolean"
+                    }
+                }
+            }
+        }"""])
+
+    Test.fromJson """["hello", 45, true, false]"""
+    |> conversion.Reader
+    |> Test.shouldEqual (Test.dictToProperty [
+        "item1", Pulumi.Provider.PropertyValue("hello")
+        "item2", Pulumi.Provider.PropertyValue(45)
+        "rest", Test.listToProperty [
+            Pulumi.Provider.PropertyValue(true)
+            Pulumi.Provider.PropertyValue(false)
+        ]
+    ])
+
+    Test.dictToProperty [
+        "item1", Pulumi.Provider.PropertyValue("goodbye")
+        "item2", Pulumi.Provider.PropertyValue(47)
+    ]
+    |> conversion.Writer
+    |> Test.toJson
+    |> Test.shouldJsonEqual """["goodbye", 47]"""
+
+    let exc = Assert.Throws<exn>(fun () ->
+        Test.fromJson """[false]"""
+        |> conversion.Reader
+        |> ignore)
+    exc.Message |> Test.shouldEqual "Value is \"boolean\" but should be \"string\""
+
+    let exc = Assert.Throws<exn>(fun () ->
+        Test.fromJson """[1.5]"""
+        |> conversion.Reader
+        |> ignore)
+    exc.Message |> Test.shouldEqual "Value is \"number\" but should be \"string\""
+    
+    let exc = Assert.Throws<exn>(fun () ->
+        Test.dictToProperty [
+            "item1", Pulumi.Provider.PropertyValue(2)
+        ]
+        |> conversion.Writer
+        |> ignore)
+    exc.Message |> Test.shouldEqual "Value is \"integer\" but should be \"string\""
+    
+    let exc = Assert.Throws<exn>(fun () ->
+        Test.dictToProperty [
+            "item1", Pulumi.Provider.PropertyValue(1)
+            "item2", Pulumi.Provider.PropertyValue(2)
+        ]
+        |> conversion.Writer
+        |> ignore)
+    exc.Message |> Test.shouldEqual "Value is \"integer\" but should be \"string\""
